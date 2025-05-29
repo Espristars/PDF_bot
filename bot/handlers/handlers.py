7b55457f50f767043a20f93f6c68e5a7bdb7cc3e
Esp <demid2207@gmail.com>
2025-05-29 23:09:43 +0300

    Added parsing table from photo

--- bot/handlers/handlers.py
+++ bot/handlers/handlers.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.filters import Command
@@ -6,7 +8,7 @@ import os
 
 
 from bot.handlers.commands import handle_start
-from bot.service_functions import file_convert_pdf_to_json
+from bot.service_functions import file_convert_pdf_to_json, file_convert_photo_to_text
 from config import bot
 
 router = Router()
@@ -14,8 +16,32 @@ router = Router()
 router.message.register(handle_start, Command("start"))
 
 
+async def handle_photo(msg: Message):
+
+    file = await msg.bot.get_file(msg.document.file_id)
+    file_path = file.file_path
+
+    os.makedirs("photos", exist_ok=True)
+    local_filename = os.path.join("photos", msg.document.file_name)
+    await bot.download_file(file_path, local_filename)
+
+    path_json = await file_convert_photo_to_text(local_filename)
+
+    os.remove(local_filename)
+
+    document = FSInputFile(path_json)
+
+    await msg.answer_document(document)
+
+    os.remove(path_json)
+
+
 @router.message(F.content_type == 'document')
 async def handle_file(msg: Message):
+
+    if msg.document.file_name.lower().endswith(".jpeg"):
+        asyncio.create_task(handle_photo(msg))
+        return
     if not msg.document.file_name.lower().endswith(".pdf"):
         await msg.reply("Пожалуйста, отправьте файл в формате *.pdf!")
         return
--- bot/service_functions.py
+++ bot/service_functions.py
@@ -3,6 +3,8 @@ import pandas
 import json
 import re
 import os
+from PIL import Image
+import pytesseract
 
 def clean_numbers(x):
     x = re.sub(r'[^\d.]', '', str(x))
@@ -28,5 +30,44 @@ async def file_convert_pdf_to_json(path_file: str) -> str:
 
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(result, f, ensure_ascii=False, indent=2)
+
+    return filename
+
+
+async def file_convert_photo_to_text(path_file: str) -> str:
+
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+    text = pytesseract.image_to_string(Image.open(path_file), lang="rus+eng")
+
+    filename = await file_convert_text_to_json(text)
+
+    return filename
+
+
+async def file_convert_text_to_json(text: str) -> str:
+    lines = [line.strip() for line in text.strip().splitlines() if line.strip()]
+
+    client_id_idx = lines.index("client_id")
+    fio_idx = lines.index("client_FlO")
+    income_idx = lines.index("credit_income")
+
+    client_ids = [line.replace(" ", "") for line in lines[client_id_idx + 1:client_id_idx + 6]]
+    fios = lines[fio_idx + 1:fio_idx + 6]
+    incomes = lines[income_idx + 1:income_idx + 6]
+
+    result = []
+    for i in range(5):
+        result.append({
+            client_ids[i]: {
+                "client_FIO": fios[i],
+                "credit_income": float(incomes[i])
+            }
+        })
+
+    filename = os.path.join("photos", "file.json")
+
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(result, f, indent=2, ensure_ascii=False)
 
     return filename
